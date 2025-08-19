5241abf826434b67d54842ae3720bee992d07dec
Gurudath <bandagurudath@gmail.comcd>
2025-08-19 18:46:32 +0530

    commit

--- outputs.tf
+++ outputs.tf
@@ -18,8 +18,12 @@ output "database_subnet_ids" {
   value = aws_subnet.database[*].id
 }
 
-output "aws_db_subnet_group" {
-  value = aws_db_subnet_group.default[*].id
+output "db_subnet_group_id" {
+  value = aws_db_subnet_group.default.id
+}
+
+output "db_subnet_group_name" {
+  value = aws_db_subnet_group.default.name
 }
 
 output "aws_internet_gateway_id" {
